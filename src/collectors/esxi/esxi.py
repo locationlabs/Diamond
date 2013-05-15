1467d5d959dd70059ccbdaf3854429e0a98cdaa3
David Morton <david.morton@locationlabs.com>
2013-05-14 19:14:55 -0700

    Adding configuration/usage instructions

--- src/collectors/esxi/esxi.py
+++ src/collectors/esxi/esxi.py
@@ -4,11 +4,18 @@ A collector that fetches data from an ESXI vSphere hypervisor
 
 depends on pysphere
 
-Configuration requires additional parameters:
+A configuration file named EsxiCollector.conf should contain the following parameters:
+
+enabled = True
+path_suffix = ""
+measure_collector_time = False
+byte_unit = byte,
+simple = False
+percore = True
 
 hosts = <host1>, <host2>, 
 username = <username>
-password = <CTE.bhg
+password = <password>
 
 Testing via the command line:
 
@@ -20,15 +27,23 @@ from pysphere import VIServer
 
 
 class EsxiCollector(diamond.collector.Collector):
-
-     def collect(self):
+    def collect(self):
         """
-        Overrides the Collector.collect method
+        A subclass of Diamond's Collector that publishes pysphere metrics
         """
+        # For each host defined in EsxiCollector.conf
         for h in self.config['hosts']:
-            server = VIServer()
-            server.connect(h, self.config['username'], self.config['password'])
-            host = server.get_hosts().keys()[0]  # ugg.. an ambiguous nested class in dynamically generated code
-            pm = server.get_performance_manager()
-            for k, v in pm.get_entity_counters(host).items():
-                self.publish('%s.%s' % (h, k), v)
+
+             # Create an instance of VIServer
+             server = VIServer()
+
+             # Authenticate using credentials in EsxiCollector.conf
+             server.connect(h, self.config['username'], self.config['password'])
+             host = server.get_hosts().keys()[0]  # ugg.. an ambiguous nested class in dynamically generated code
+
+             # Get a reference to PerformanceManager
+             pm = server.get_performance_manager()
+
+             # And publish all values available
+             for k, v in pm.get_entity_counters(host).items():
+                  self.publish('%s.%s' % (h, k), v)
